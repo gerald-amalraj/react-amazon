f729026e6b6edb95fa64777e56bbcf341defd674
Gerald Amalraj <gerald.amalraj@gmail.com>
2021-08-12 22:07:45 +0530

    final checkin

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-15-206-211-103.ap-south-1.compute.amazonaws.com:8080/api",
+  baseURL: "http://ec2-13-126-235-222.ap-south-1.compute.amazonaws.com:8080/api",
   headers: {
     "Content-type": "application/json"
   }
