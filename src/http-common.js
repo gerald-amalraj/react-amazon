18d503a7efff79806c65875cf0e3af8b82e17d95
Gerald Amalraj <gerald.amalraj@gmail.com>
2021-08-14 11:23:32 +0530

    IP change

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://ec2-13-126-235-222.ap-south-1.compute.amazonaws.com:8080/api",
+  baseURL: "http://3.7.39.76:8080/api",
   headers: {
     "Content-type": "application/json"
   }
